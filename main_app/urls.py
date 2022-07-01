8eba413b0867d7d79fd16857d9c7dca4233ad2e6
Jansher Marri <janshermarri@yahoo.com>
2022-07-01 19:32:30 +0500

    BS-4: Create DRF serializers for Project, Bug, Comment (#9)
    Changes:
    
        1. Created serializers for Project, Bug, Comment
    
    Tested by:
    Not tested yet..

--- main_app/urls.py
+++ main_app/urls.py
@@ -1,6 +1,10 @@
-from django.urls import path
-
+from django.urls import path, include
+from rest_framework import routers
 from main_app import views
 
+# Create routes for models
+router = routers.SimpleRouter()
+router.register(r'projects', views.ProjectViewSet, basename='projects')
 urlpatterns = [
+    path('/api/', include((router.urls, 'main_app'))),
 ]

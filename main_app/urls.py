c92c6624da7f5049c1108616070c02922a7c558b
janshermarri <janshermarri@yahoo.com>
2022-07-01 19:29:18 +0500

    BS-4-update

--- main_app/urls.py
+++ main_app/urls.py
@@ -2,6 +2,7 @@ from django.urls import path, include
 from rest_framework import routers
 from main_app import views
 
+# Create routes for models
 router = routers.SimpleRouter()
 router.register(r'projects', views.ProjectViewSet, basename='projects')
 urlpatterns = [

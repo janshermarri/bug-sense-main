f9226f231ecdd801b1ab74a6668c60eca6c51bbe
rajBinMoti <raj20032003@gmail.com>
2022-07-01 17:39:34 +0500

    Bug Model

--- main_app/models.py
+++ main_app/models.py
@@ -17,3 +17,22 @@ class Project(models.Model):
     def __str__(self):
         return self.project_name + ' ' + self.project_status
 
+
+class Bug(models.Model):
+    SEVERITY_CHOICES = [
+        ('NORMAL', 'Normal'),
+        ('URGENT', 'Urgent'),
+        ('LAZY', 'Lazy'),
+    ]
+
+    project_id = models.ForeignKey(Project, on_delete=models.CASCADE)
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    bug_name = models.CharField(max_length=1024)
+    bug_description = models.TextField(blank=True, max_length=2048)
+    bug_status = models.CharField(max_length=10, choices=STATUS_CHOICES)
+    bug_severity = models.CharField(max_length=10, choices=SEVERITY_CHOICES)
+    bug_assignees = models.CharField(max_length=1024)
+
+    def __str__(self):
+        return self.bug_name + " " + self.user_id + " " + self.project_id
+

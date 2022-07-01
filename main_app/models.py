2cd6a6511d714fe93612fbb0f976ab763ba5356f
rajBinMoti <raj20032003@gmail.com>
2022-07-01 17:39:11 +0500

    Project model updated

--- main_app/models.py
+++ main_app/models.py
@@ -1,6 +1,19 @@
 from django.db import models
 
+STATUS_CHOICES = [
+    ('TODO', 'To Do'),
+    ('DOING', 'Doing'),
+    ('IN_REVIEW', 'In Review'),
+    ('DONE', 'Done')
+]
+
+
 class Project(models.Model):
-    name = models.CharField(max_length=100)
-    description = models.TextField(blank=True, max_length=1500)
-    technologies_used = models.TextField(blank=True, max_length=1000)
+    project_name = models.CharField(max_length=100)
+    project_description = models.TextField(blank=True, max_length=1500)
+    project_technologies_used = models.TextField(blank=True, max_length=1000)
+    project_status = models.CharField(max_length=9, choices=STATUS_CHOICES)
+
+    def __str__(self):
+        return self.project_name + ' ' + self.project_status
+
